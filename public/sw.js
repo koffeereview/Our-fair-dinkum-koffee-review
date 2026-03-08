dbb45b89226d62adb65f412e46e4f666b8909abc
koffeereview <koffeereview@gmail.com>
2026-03-08 19:39:36 +1000

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "koffee-review-v1";
+const urlsToCache = ["/", "/logo.jpg"];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
