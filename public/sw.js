78baf9d0f24fdb0c46f3d60cdd50f3382714224f
koffeereview <koffeereview@gmail.com>
2026-03-10 18:43:28 +1000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = "koffee-review-v1";
-const urlsToCache = ["/", "/logo.jpg"];
-
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function() {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(key) {
+        return caches.delete(key);
+      }));
     })
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
