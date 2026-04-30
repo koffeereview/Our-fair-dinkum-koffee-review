fe4bb16017932708a0ecc0a66928843e0881bde9
koffeereview <koffeereview@gmail.com>
2026-04-30 22:38:00 +1000

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -3,7 +3,7 @@
     {
       "source": "/(.*)",
       "headers": [
-        { "key": "X-Content-Type-Options", "value": "nosniff" },
+        { "key": "X-Content-Type-Options", "value": "nosniff" }, 
         { "key": "X-Frame-Options", "value": "DENY" },
         { "key": "X-XSS-Protection", "value": "1; mode=block" }
       ]
