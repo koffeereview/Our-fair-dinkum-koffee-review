ee739e816af5b43736ea0819a09984fe54d0654c
koffeereview <koffeereview@gmail.com>
2026-05-07 20:25:04 +1000

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -2,3 +2,4 @@ export default function handler(req, res) {
   res.setHeader("Content-Type", "application/xml; charset=utf-8");
   res.status(200).send('<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>https://koffeereview.com.au/</loc>\n    <priority>1.0</priority>\n  </url>\n</urlset>');
 }
+ 
